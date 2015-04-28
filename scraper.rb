09ac04673aa174cfac87c30d026739d841939983
Henare Degan <henare.degan@gmail.com>
2015-04-28 14:46:17 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,3 @@
 require 'open-uri'
 
-puts open("https://api.morph.io/morph-test-scrapers/test-ruby/data.json?query=select * from 'data' limit 10&key=" + ENV["MORPH_API_KEY"]).read
+puts open("https://api.morph.io/morph-test-scrapers/test-ruby/data.json?&query=select%20*%20from%20%27data%27%20limit%2010&key=" + ENV["MORPH_API_KEY"]).read
