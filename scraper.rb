c5a481f1c3859e4a3b7724f5112d42fc72104edf
Henare Degan <henare.degan@gmail.com>
2015-04-28 14:44:09 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,3 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'open-uri'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on morph.io for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+puts open("https://api.morph.io/morph-test-scrapers/test-ruby/data.json?query=select * from 'data' limit 10&key=" + ENV["MORPH_API_KEY"]).read
